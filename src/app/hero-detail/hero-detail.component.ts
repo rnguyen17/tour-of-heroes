8d52d19cdc1a041ea12053f87fa1f2106af11bd3
Raj <raj@d25sj17hgq17im.dv6598.homedepot.com>
2017-11-08 16:32:20 -0600

    Abstract out hero details into separate component

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { Hero } from '../heroes/hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
